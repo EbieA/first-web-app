931585ecdbfd11b4c4fb8ab3bacfa2df29b4d380
Ebie <adewumiebenezer7@gmail.com>
2023-10-21 21:38:48 +0100

    header start

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import {motion} from 'framer-motion';
 
+import {images} from '../../constants'
 import './Header.scss'
+
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='app__header app__flex'>
+      <motion.div 
+      whileInView={{x: [-100, 0], opacity: [0,1]}}
+      transition={{duration: 0.5}}
+      className='app__header-info'
+      >
+      <div className='app__header-badge'>
+      <div className='badge-cmp app__flex'>
+        <span>👋</span>
+        <div style={{marginLeft: 20}}>
+          <p className='p-text'>Hello, I am</p>
+          <h1 className='hea-text'>Ebenezer</h1>
+        </div>
+      </div>
+      </div>
+      </motion.div>
+    </div>
   )
 }
