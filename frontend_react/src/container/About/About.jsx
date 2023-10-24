d87e56c080b0f7510ac25d0b93d3e59b2b7f8424
Ebie <adewumiebenezer7@gmail.com>
2023-10-24 09:34:40 +0100

    backend query

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,28 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
 
-
-
 import './About.scss'
+import { urlFor, client } from '../../client'
 
-const abouts = [
-   { title: 'Web Developer', description: 'I am a good web developer', imgUrl: ''},
-   { title: 'Frontend Developer', description: 'I am a good web developer', imgUrl: ''},
-   { title: 'Backend Developer', description: 'I am a good web developer', imgUrl: ''},
-   { title: 'Mobile Developer', description: 'I am a good web developer', imgUrl: ''}
-];
 
 export default function About() {
+
+  const [abouts, setAbouts] = useState([]);
+
+
+  useEffect(() => {
+    const query = '*[_type == "about"]';
+
+    client.fetch(query)
+    .then((data) => setAbouts(data))
+  }, [])
+
   return (
     <>
-      <h2 className='head-text'> I Know That 
-      <span>Good Design</span> 
-      <br />
-      means 
-      <span>Good Business</span>
-      </h2>
+      <h2 className='head-text'> I Know That <span>Good Dev </span> <br /> means <span> Good Business</span></h2>
 
-      <div className="app__profile">
+      <div className="app__profiles">
         {abouts.map((about, index) => (
           <motion.div
           whileInView={{opacity: 1}}
@@ -30,7 +29,7 @@ export default function About() {
           transition={{duration:0.5, type: 'tween'}}
           className='app__profile-item'
           >
-          <img src={about.imgUrl} alt="" />
+          <img src={urlFor(about.imgUrl)} alt="" />
           <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
           <p className='p-text' style={{marginTop: 10}}>{about.description}</p>
           </motion.div>
