3867ab57c138c4d068fffe2eac51fa68af59c959
Ebie <adewumiebenezer7@gmail.com>
2023-10-27 11:52:57 +0100

    footer done

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function NavigationDots({active}) {
   return (
     <div className='app__navigation'>
-         {['home', 'about', 'work', 'skills', 'contact', 'testimonials', ].map((item) => (
+         {['home', 'about', 'work', 'skills', 'testimonial', 'contact', ].map((item) => (
             <a 
             href={`#${item}`} 
-            key={item }
+            key={item}
             className='app__navigation-dot'
             style={active === item ? { backgroundColor: '#313BAC'} : { }}
             />
